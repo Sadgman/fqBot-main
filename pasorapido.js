db53e8957f0c8d304304fae63480383c132c561d
Sadgman <angel.luis.regalado@hotmail.com>
2025-06-24 22:30:02 -0400

    refactor: optimize prapido function and enhance client readiness logic

--- pasorapido.js
+++ pasorapido.js
@@ -1,18 +1,20 @@
 import puppeteer from 'puppeteer'
 
 const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     userDataDir: './data'
 })
 export async function prapido() {
-        const page = await browser.newPage()
+    console.log('Comprobando paso rapido...')
+    const page = await browser.newPage()
 
-        await page.goto("https://clientes.pasorapido.gob.do/inicio")
-        await page.waitForSelector('h4', { visible: true })
-        const precio = await page.evaluate(async () =>{
-            const p = document.querySelectorAll("h4")[1].textContent
-            return p
-        })
+    await page.goto("https://clientes.pasorapido.gob.do/inicio")
+    await page.waitForSelector('h4', { visible: true })
+    const precio = await page.evaluate(async () =>{
+        const p = document.querySelectorAll("h4")[1].textContent
+        return p
+    })
     const total = parseInt((await precio).match(/[\d.]+/g).join(''));
+    browser.close()
     return total
 }
