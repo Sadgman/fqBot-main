25b0a7e6165d2ce587604f0855a0782d28266e02
Sadgman <angel.luis.regalado@hotmail.com>
2025-07-09 09:01:11 -0400

    feat: integrate dotenv for environment variable management and enhance login handling in prapido function

--- pasorapido.mjs
+++ pasorapido.mjs
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer'
-
+import dotenv from 'dotenv'
+dotenv.config()
 const browser = await puppeteer.launch({
     headless: true,
     userDataDir: './data'
@@ -8,9 +9,25 @@ const page = await browser.newPage()
 
 export async function prapido() {
     console.log('Comprobando paso rapido...')
+    page.on('framenavigated', async (pestana) => {
+        if(pestana == page.mainFrame()){
+            const urllogin = pestana.url()
+            if(urllogin === "https://clientes.pasorapido.gob.do/login"){
+                    const nombre = "input[name='email']"
+                    const contra = "input[name='password']"
+                    await page.waitForSelector(nombre)
+                    await page.type(nombre, process.env.nombre)
+                    await page.type(contra, process.env.contra)
+                    page.click("input[type='checkbox']")
+                    page.click("button[type='submit']")
+                    await page.waitForNavigation()
+                    await page.waitForNetworkIdle()
+            }
+        }
+    })
     try{
-        if(page?.url === "https://clientes.pasorapido.gob.do/inicio") page.reload();
         await page.goto("https://clientes.pasorapido.gob.do/inicio")
+        
         await page.waitForSelector('h4', { visible: true })
         const precio = await page.evaluate(async () =>{
             const p = document.querySelectorAll("h4")[1].textContent
