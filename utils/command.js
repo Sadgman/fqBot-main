0299c63f4e4db5cc69c675329ce87952fd3fca79
Sadgman <angel.luis.regalado@hotmail.com>
2025-10-16 22:04:02 -0400

    feat: add windowsHide option to exec for improved command execution on Windows

--- utils/command.js
+++ utils/command.js
@@ -2,7 +2,7 @@ import { exec} from 'child_process';
 
 export default async function runCommand(command) {
     return new Promise((resolve, reject) => {
-        exec(command, {encoding: 'UTF-8', shell: false}, (error, stdout, stderr) => {
+        exec(command, {encoding: 'UTF-8', shell: false, windowsHide: true}, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
             } else {
